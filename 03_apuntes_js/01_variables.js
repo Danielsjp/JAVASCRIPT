7a5b138e0314f7e6ec8edfb0540190aa2e3785f2
daniel.sanchez <sjp.daniel@gmail.com>
2025-03-12 17:19:00 +0100

    hello

---
+++ 03_apuntes_js/01_variables.js
@@ -0,0 +1,23 @@
+// VARIABLES
+//javascript pluggin para autocompletar el codigo
+//quokka plugin para avisar en tiempo de ejecucion
+var num1 = 1
+
+var num1 = 8
+let num2 = 5
+
+const mensaje = "HOLA";
+// mensaje = "Hola"
+let num3
+
+//clg es el atajo de teclado
+
+console.log(typeof num3);
+num3 = 2.5
+console.log(typeof num3);
+
+// Descructuring
+
+let [txt1, txt2, txt3] =  ["a", "b", "c"]
+console.log(txt1, txt2, txt3);
+console.log(typeof txt1);
