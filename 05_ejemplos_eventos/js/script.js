34d278899620c825fe5759be6219dcec3a756afc
daniel.sanchez <sjp.daniel@gmail.com>
2025-03-18 16:41:55 +0100

    hello

--- 05_ejemplos_eventos/js/script.js
+++ 05_ejemplos_eventos/js/script.js
@@ -1,1 +1,17 @@
-alert("ok")
+
+// Obtener el nodo con id "cabriolet"
+// const cabriolet = document.getElementById('cabriolet')
+const cabriolet = document.querySelector('#cabriolet')
+const alquiler = document.querySelector("#alquiler")
+const importe = document.querySelector("main section:nth-child(2)")
+
+cabriolet.addEventListener("click", () => {
+    let dias = prompt("¿Días de alquiler?")
+    let precio = cabriolet.querySelector('ul li:last-child').textContent
+    precio = precio.split('€')[0]
+    alert(precio)
+    importe.style.display = "block"
+    let alquilerTotal = parseFloat(precio) * parseInt(dias)
+    let mensajeUsuario = `${dias} días de alquiler x ${precio}€/día = <span class="bold">${alquilerTotal}€</span>`
+    alquiler.innerHTML = mensajeUsuario
+} )
