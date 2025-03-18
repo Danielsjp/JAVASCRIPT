1bb4f6cf0be6c5f4007f7f4920c0ccbe3289e756
daniel.sanchez <sjp.daniel@gmail.com>
2025-03-18 17:59:44 +0100

    test

--- 05_ejemplos_eventos/js/script.js
+++ 05_ejemplos_eventos/js/script.js
@@ -2,16 +2,31 @@
 // Obtener el nodo con id "cabriolet"
 // const cabriolet = document.getElementById('cabriolet')
 const cabriolet = document.querySelector('#cabriolet')
-const alquiler = document.querySelector("#alquiler")
-const importe = document.querySelector("main section:nth-child(2)")
+const alquiler = document.querySelector('#alquiler')
+const importe = document.querySelector('main section:nth-child(2)')
+
 
 cabriolet.addEventListener("click", () => {
     let dias = prompt("¿Días de alquiler?")
     let precio = cabriolet.querySelector('ul li:last-child').textContent
     precio = precio.split('€')[0]
-    alert(precio)
+    // alert(precio)
     importe.style.display = "block"
     let alquilerTotal = parseFloat(precio) * parseInt(dias)
-    let mensajeUsuario = `${dias} días de alquiler x ${precio}€/día = <span class="bold">${alquilerTotal}€</span>`
+    // let mensajeUsuario = `${dias} días de alquiler x ${precio}€/día = ${alquilerTotal.toFixed(2)}€`
+    // alquiler.textContent = mensajeUsuario
+    let mensajeUsuario = `${dias} días de alquiler x ${precio}€/día = <span class="bold">${alquilerTotal.toFixed(2)}€</span>`
     alquiler.innerHTML = mensajeUsuario
 } )
+let PrecioFinal = 0
+function alquilarVehiculo(precioDia) {
+    let dias = prompt("¿Días de alquiler?")
+    importe.style.display = "block"
+    
+    let alquilerTotal = parseFloat(precioDia) * parseInt(dias)
+    let mensajeUsuario = `${dias} días de alquiler x ${precioDia}€/día = <span class="bold">${alquilerTotal.toFixed(2)}€</span>`
+    alquiler.innerHTML += `<p>${mensajeUsuario}</p>`
+    PrecioFinal += alquilerTotal;
+    totalPrecio.innerHTML = PrecioFinal;
+}
+
