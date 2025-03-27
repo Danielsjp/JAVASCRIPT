40e3a33ee2874670b382f29df9f11c66acf829a1
daniel.sanchez <sjp.daniel@gmail.com>
2025-03-27 17:16:01 +0100

    hello

--- API_METEO/12_main.js
+++ API_METEO/12_main.js
@@ -2,6 +2,7 @@ import text from "./12_texto.js"
 console.log(text)
 
 const btnTexto = document.getElementById("btntexto")
+
 const divTexto = document.getElementById("divTexto")
 
 btnTexto.addEventListener("click", ()=> {
@@ -15,4 +16,23 @@ btnTexto.addEventListener("click", ()=> {
         divTexto.textContent = data
 
     })
+    .catch(error => {
+
+        console.log("Error:", error)
+    })
+    .finally( () => {
+
+        console.log("Programa finalizado");
+    })
+})
+
+const btnDatos = document.getElementById("btnDatos")
+const divDatos = document.getElementById("divDatos")
+
+btnDatos.addEventListener("click", () => {
+        fetch("12_datos.json")
+        .then(data => data.json())
+        .then(res => {
+            console.table(res);
+        }).catch(error => console.error("Error", error))
 })
