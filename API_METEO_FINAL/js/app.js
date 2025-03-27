d2784cdee9b5ef6649adbf8796ca1786caf1d89d
daniel.sanchez <sjp.daniel@gmail.com>
2025-03-27 19:28:39 +0100

    hello

--- API_METEO_FINAL/js/app.js
+++ API_METEO_FINAL/js/app.js
@@ -1,24 +1,25 @@
 // import key from "./"
 const apiKey = "7268048e6829b1df5dfcc5e82529ae22"; // Tu API Key
-const urlBase = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=es&q=";
-
+// const urlBase = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=it&q=";
+const urlBase = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
 const form = document.getElementById("weatherForm");
 const inputCiudad = document.getElementById("cityInput");
+const idioma_1 = document.getElementById("languageSelect")
 const divDatos = document.getElementById("divDatos");
 icono = "https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/"
 
 form.addEventListener("submit", (event) => {
     event.preventDefault(); // Evita que la página se recargue al enviar el formulario
-
+    const idioma = idioma_1.value;
     const ciudad = inputCiudad.value.trim(); // Obtener la ciudad y eliminar espacios en blanco
     if (ciudad === "") return; // No hacer nada si el input está vacío
 
-    obtenerClima(ciudad);
+    obtenerClima(ciudad, idioma);
 });
 
-function obtenerClima(ciudad) {
-    const url = `${urlBase}${ciudad}&appid=${apiKey}`;
+function obtenerClima(ciudad, idioma) {
+    const url = `${urlBase}&lang=${idioma}&q=${ciudad}&appid=${apiKey}`;
 
     fetch(url)
         .then(response => response.json())
